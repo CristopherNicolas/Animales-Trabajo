82404010a63b2042287ec6c7b686119ec91c527b
lanchox <78050667+lanchox@users.noreply.github.com>
2022-04-27 17:04:17 -0400

    Pruebas de contadores

--- Assets/Core/farmeo/GanarOro.cs
+++ Assets/Core/farmeo/GanarOro.cs
@@ -1,12 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GanarOro : MonoBehaviour
 {
-    public void GanarOr()
+    public Text Oro;
+    private void Start()
     {
+        Oro = GetComponent<Text>();
+        StartCoroutine(GanarOr());
+    }
+    private void Update()
+    {
+        
+    }
+    public void FixedUpdate()
+    {
+        Oro.text = "$" + GameManager.instance.dinero;
+    }
+    IEnumerator GanarOr()
+    {
+        yield return new WaitForSeconds(1f);
         GameManager.instance.dinero += 1;
+        StartCoroutine(GanarOr());
     }
     
 }
