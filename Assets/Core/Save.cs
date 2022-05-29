4c320a44d14e4a2d7e8778489c2e02b28752cab2
lanchox <78050667+lanchox@users.noreply.github.com>
2022-05-29 14:11:39 -0400

    V 0.994
    Descripciones agregadas en Save

--- Assets/Core/Save.cs
+++ Assets/Core/Save.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 [System.Serializable]
 public class Save 
 {
-    public float dinero,perros,gatos,patos;
+    public List<string> animalEnTerreno; //Nominados en la script TerrenoManager: public string AnimalTerreno1, AnimalTerreno2,AnimalTerreno3,AnimalTerreno4,AnimalTerreno5;animalTerreno6.
+    //opciones a usar: "Gato" "Perro" "Pato" "Tigre" "Zorro" "Pollo".
+
+    public List<GameObject> spritesEnTerrenos; //Nominados en la script TerrenoManager: public GameObject AnimalSprite1, AnimalSprite2,AnimalSprite3,AnimalSprite4,AnimalSprite5;animalSprite6.
+    public List<int> animalesPorTerreno; //Nominados en la script Tienda: public int AnimalesEnT1, AnimalesEnT2,AnimalesEn3,AnimalesEnT4,AnimalesEnT5;AnimalesEnT6.
+    public List<float> gananciasPorTerreno;//Nominados en la script Terreno: GananciasT1,GananciasT2,GananciasT3,GananciasT4,GananciasT5,GananciasT6.
     public List<Animal> animalesEnTerrenos;
+    public float dinero;
 }
